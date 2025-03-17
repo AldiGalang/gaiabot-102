4be739a64e43b44bf486d69b78b5874db0465964
RisolMayoETH <121712363+AldiGalang@users.noreply.github.com>
2025-03-17 19:18:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-
+git clone https://github.com/AldiGalang/gaiabot-102.git
+sudo apt update && sudo apt install screen python3 python3.12-venv -y
+screen -dmS bot bash -c 'cd gaiabot-102 && python3 -m venv myenv && source myenv/bin/activate && pip install -r requirements.txt && echo "Bot is ready. Please edit file_api_keys.txt manually."'
