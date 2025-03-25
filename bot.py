a26308ddfe31f9de7ba26116eef5376e673a888d
RisolMayoETH <121712363+AldiGalang@users.noreply.github.com>
2025-03-25 21:10:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ async def main():
                 failed += 1
             
             if index < total_questions:
-                await countdown_next_questions(random.randint(3, 7))  # Tunggu sebelum pertanyaan berikutnya
+                await countdown_next_questions(30)  # Tunggu sebelum pertanyaan berikutnya
         
         print(f"{Fore.LIGHTBLUE_EX}🎯 Session {Fore.LIGHTWHITE_EX}{cycle} {Fore.LIGHTBLUE_EX}completed!")
         print(f"{Fore.LIGHTGREEN_EX}✅ Successfully answered: {Fore.LIGHTWHITE_EX}{answered}")
